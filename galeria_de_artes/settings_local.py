405968073c8894f9a3f444cd3ba0c59925607c12
Daniel Michelon De Carli <carli@ufsm.br>
2016-08-26 16:22:20 -0300

    Alteraçoes no servidor de produção

--- galeria_de_artes/settings_local.py
+++ galeria_de_artes/settings_local.py
@@ -6,7 +6,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'galeria_de_arte',
+        'USER':'root',
+        'PASSWORD':'$Ervico$2013'
     }
 }
